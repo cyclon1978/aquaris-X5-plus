f61f9c9a94e442f6728a269df4a5d8e2ca16bc5b
Florian Westphal <fw@strlen.de>
2016-03-22 18:02:50 +0100

    netfilter: x_tables: make sure e->next_offset covers remaining blob size
    commit 6e94e0cfb0887e4013b3b930fa6ab1fe6bb6ba91 upstream.
    
    Otherwise this function may read data beyond the ruleset blob.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- net/ipv6/netfilter/ip6_tables.c
+++ net/ipv6/netfilter/ip6_tables.c
@@ -738,7 +738,7 @@ check_entry_size_and_hooks(struct ip6t_entry *e,
 	if ((unsigned long)e % __alignof__(struct ip6t_entry) != 0 ||
 	    (unsigned char *)e + sizeof(struct ip6t_entry) >= limit ||
 	    (unsigned char *)e + e->next_offset > limit) {
-		duprintf("Bad offset %pK\n", e);
+		duprintf("Bad offset %p\n", e);
 		return -EINVAL;
 	}
 
@@ -1503,7 +1503,7 @@ check_compat_entry_size_and_hooks(struct compat_ip6t_entry *e,
 	if ((unsigned long)e % __alignof__(struct compat_ip6t_entry) != 0 ||
 	    (unsigned char *)e + sizeof(struct compat_ip6t_entry) >= limit ||
 	    (unsigned char *)e + e->next_offset > limit) {
-		duprintf("Bad offset %pK, limit = %pK\n", e, limit);
+		duprintf("Bad offset %p, limit = %p\n", e, limit);
 		return -EINVAL;
 	}
 
