4080369f72fc5d8beb948b704c51c131a195af19
andip71 <andreasp@gmx.de>
2016-09-25 15:54:03 +0530

    Update dynamic fsync to V2.0

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -25,6 +25,7 @@
 #include <linux/clk.h>
 #include <linux/uaccess.h>
 #include <linux/pm_qos.h>
+#include <linux/lcd_notify.h>
 
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
@@ -2210,6 +2211,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
@@ -2224,8 +2226,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 #ifdef CONFIG_STATE_NOTIFIER
 		state_resume();
 #endif
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		power_state = (int) (unsigned long) arg;
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
@@ -2239,6 +2243,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 #ifdef CONFIG_STATE_NOTIFIER
 		state_suspend();
 #endif
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
--- fs/sync.c
+++ fs/sync.c
@@ -22,8 +22,7 @@ bool fsync_enabled = true;
 module_param(fsync_enabled, bool, 0755);
 
 #ifdef CONFIG_DYNAMIC_FSYNC
-extern bool power_suspend_active;
-extern bool dyn_fsync_active;
+#include <linux/dyn_sync_cntrl.h>
 #endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
@@ -113,6 +112,7 @@ void sync_filesystems(int wait)
 	iterate_bdevs(fdatawait_one_bdev, NULL);
 }
 #endif
+
 
 /*
  * Sync everything. We start by waking flusher threads so that most of
@@ -211,16 +211,12 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 		return 0;
 
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && suspend_active))
 		return 0;
-	else {
 #endif
 	if (!file->f_op || !file->f_op->fsync)
 		return -EINVAL;
 	return file->f_op->fsync(file, start, end, datasync);
-#ifdef CONFIG_DYNAMIC_FSYNC
-	}
-#endif
 }
 EXPORT_SYMBOL(vfs_fsync_range);
 
@@ -264,9 +260,8 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 		return 0;
 
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && suspend_active))
 		return 0;
-	else
 #endif
 	return do_fsync(fd, 0);
 }
@@ -276,6 +271,10 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 	if (!fsync_enabled)
 		return 0;
 
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
+		return 0;
+#endif
 	return do_fsync(fd, 1);
 }
 
@@ -349,11 +348,6 @@ EXPORT_SYMBOL(generic_write_sync);
 SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 				unsigned int, flags)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
-		return 0;
-	else {
-#endif
 	int ret;
 	struct fd f;
 	struct address_space *mapping;
@@ -362,6 +356,11 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 
 	if (!fsync_enabled)
 		return 0;
+
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
+		return 0;
+#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
@@ -436,9 +435,6 @@ out_put:
 	fdput(f);
 out:
 	return ret;
-#ifdef CONFIG_DYNAMIC_FSYNC
-	}
-#endif
 }
 
 /* It would be nice if people remember that not all the world's an i386
@@ -447,9 +443,8 @@ SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && suspend_active))
 		return 0;
-	else
 #endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
