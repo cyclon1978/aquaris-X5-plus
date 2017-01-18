MotorolaMobilityLLC/kernel-msm

arm64: avoid segfault on el0 cache maintenance
It is found that "dc civac" from el0 on a non-COW RO VA results in a
segmentation fault, while as per ARM ARM, for el0 only read access
permission is needed on a VA to perform clean and invalidate. This bug
was introduced by commit 3fbe6bc28a6b ("arm64: Allow cache maintenance
operations to trigger write faults"), which enabled write faults on all
cache maintenance faults.
The original intention of the above commit was to trigger write faults
only for cache maintenance operations performed by EL1 on user VA. But
it introduced this side effect of generating a write fault on EL0 cache
maintenance ops also when a read permission is sufficient for operation
to succeed. This results in a segmentation fault when the VA is part of
region marked as PROT_READ.
So trigger write faults only when the cache maintenance fault is generated
in non-user mode. Still, there can be a case where kernel performs a cache
maintenance op on a PROT_READ mapped VA which will result in a kernel
fault. But that looks invalid since there is no need for a kernel driver
to perform a cache maintenance on a RO buffer and such cases if they exist
need to be fixed.

Change-Id: I001ca0473c37fcf713201386b5ed6a6244f6b37d
Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

arch/arm64/mm/fault.c
@@ -283,7 +283,8 @@ static int __kprobes do_page_fault(unsigned long addr, unsigned int esr,
 
 	if (esr & ESR_LNX_EXEC) {
 		vm_flags = VM_EXEC;
-	} else if (esr & ESR_ELx_WNR) {
+	} else if (((esr & ESR_ELx_WNR) && !(esr & ESR_ELx_CM)) ||
+			((esr & ESR_ELx_CM) && !(mm_flags & FAULT_FLAG_USER))) {
 		vm_flags = VM_WRITE;
 		mm_flags |= FAULT_FLAG_WRITE;
 	}

cyclon1978/aquaris-X5-plus

arch/arm64/mm/fault.c
@@ -223,7 +223,8 @@ static int __kprobes do_page_fault(unsigned long addr, unsigned int esr,
 
 	if (esr & ESR_LNX_EXEC) {
 		vm_flags = VM_EXEC;
-	} else if (esr & ESR_EL1_WRITE) {
+	} else if (((esr & ESR_EL1_WRITE) && !(esr & ESR_EL1_CM)) ||
+			((esr & ESR_EL1_CM) && !(mm_flags & FAULT_FLAG_USER))) {
 		vm_flags = VM_WRITE;
 		mm_flags |= FAULT_FLAG_WRITE;
 	}