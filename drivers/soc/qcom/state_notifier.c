661246ea4c86b4297b045bacbb32dba7006f733e
Anonymous <cyclon1978@users.noreply.github.com>
2017-02-09 09:44:00 +0100

    removed "force cpu 0 for workqueue" - fails if specific cpu is offline

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -9,9 +9,10 @@
  *
  */
 
+#include <linux/state_notifier.h>
+#include <linux/notifier.h>
 #include <linux/export.h>
 #include <linux/module.h>
-#include <linux/state_notifier.h>
 
 #define DEFAULT_SUSPEND_DEFER_TIME 	10
 #define STATE_NOTIFIER			"state_notifier"
@@ -28,7 +29,7 @@ do {				\
 		pr_info(msg);	\
 } while (0)
 
-static bool enabled;
+static bool enabled = true;
 module_param_named(enabled, enabled, bool, 0664);
 static unsigned int suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME;
 module_param_named(suspend_defer_time, suspend_defer_time, uint, 0664);
@@ -96,7 +97,7 @@ void state_suspend(void)
 
 	suspend_in_progress = true;
 
-	queue_delayed_work_on(0, susp_wq, &suspend_work, 
+	queue_delayed_work(susp_wq, &suspend_work, 
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
@@ -107,7 +108,7 @@ void state_resume(void)
 	suspend_in_progress = false;
 
 	if (state_suspended)
-		queue_work_on(0, susp_wq, &resume_work);
+		queue_work(susp_wq, &resume_work);
 }
 
 static int __init state_notifier_init(void)
